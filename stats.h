c707765f4875eeafaaec02658c8680b12af2ccfa
VishnuMS13 <105206650+VishnuMS13@users.noreply.github.com>
2022-05-24 19:33:16 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,13 @@
 #include <vector>
 
+typedef struct ComputedStats
+{
+   float average;
+   float max;
+   float min;
+} trComputedStats;
+
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>& vectorInput);
+    trComputedStats m_trComputedStats;
 }
