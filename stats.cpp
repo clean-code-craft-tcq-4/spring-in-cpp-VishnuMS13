2088300483eb74eb556a08b291cc063e6dffd313
VishnuMS13 <105206650+VishnuMS13@users.noreply.github.com>
2022-05-24 22:09:30 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,21 +1,19 @@
 #include "stats.h"
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& vectorInput) {
-    //Implement statistics here
+    Stats m_trStats;
     if (vectorInput.empty()) 
     {
         m_trStats.average = 0;
         m_trStats.min = 0;
         m_trStats.max = 0;
-
     }
     else
     {
         m_trStats.average = accumulate(vectorInput.begin(), vectorInput.end(), 0.0/ vectorInput.size());
-        m_trStats.min = *min_element(vectorInput.begin(), vectorInput.end());
+        m_trStats.min = *std::min_element(vectorInput.begin(), vectorInput.end());
         m_trStats.max = *max_element(vectorInput.begin(), vectorInput.end());
     }
 
     return m_trStats;
 }
-
