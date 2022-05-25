8669a204f0b5d10b030fb2146740f723417012be
VishnuMS13 <105206650+VishnuMS13@users.noreply.github.com>
2022-05-25 14:23:21 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,15 +2,48 @@
 #include <numeric>
 #include <iostream>
 #include <bits/stdc++.h>
-#include <math.h>
+#include <algorithm>
+
+class IAlerter
+{
+public:
+    bool emailSent;
+    bool ledGlows;
+};
+
+class EmailAlert : public IAlerter
+{
+public:
+    bool emailSent{false};
+};
+
+class LEDAlert : public IAlerter
+{
+public:
+    bool ledGlows{false};
+};
+
+class StatsAlerter
+{
+    std::vector<IAlerter *> mAlerters;
+    float thresholdValue;
+
+public:
+    EmailAlert *emailAlert;
+    LEDAlert *ledAlert;
+
+    StatsAlerter(float maxThreshold, const std::vector<IAlerter *> alerters);
+    void checkAndAlert(const std::vector<float> &vectorInput);
+};
 
 struct Stats
 {
-   float average;
-   float max;
-   float min;
+    float average;
+    float max;
+    float min;
 };
 
-namespace Statistics {
-    Stats ComputeStatistics(const std::vector<float>& vectorInput);
+namespace Statistics
+{
+    Stats ComputeStatistics(const std::vector<float> &vectorInput);
 }
