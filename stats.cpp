71aeed62e5ee23f63d468f0fe6a0457163738e94
VishnuMS13 <105206650+VishnuMS13@users.noreply.github.com>
2022-05-25 14:23:17 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,18 +1,38 @@
+#include <math.h>
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<float>& vectorInput) {
+StatsAlerter::StatsAlerter(float maxThreshold, const std::vector<IAlerter *> alerters)
+{
+    thresholdValue = maxThreshold;
+    mAlerters = alerters;
+}
+
+void StatsAlerter::checkAndAlert(const std::vector<float> &vectorInput)
+{
+    if (*max_element(vectorInput.begin(), vectorInput.end()) > thresholdValue)
+    {
+        mAlerters.at(0)->emailSent = true;
+        emailAlert = reinterpret_cast<EmailAlert *>(mAlerters.at(0));
+        ledAlert = reinterpret_cast<LEDAlert *>(mAlerters.at(1));
+        emailAlert->emailSent = true;
+        ledAlert->ledGlows = true;
+    }
+}
+
+Stats Statistics::ComputeStatistics(const std::vector<float> &vectorInput)
+{
     double sum = 0.0;
     Stats m_trStats;
 
-    if (vectorInput.empty()) 
+    if (vectorInput.empty())
     {
-        m_trStats.average = nan("0.0f");
-        m_trStats.min = nan("0.0f");
-        m_trStats.max = nan("0.0f");
+        m_trStats.average = NAN;
+        m_trStats.min = NAN;
+        m_trStats.max = NAN;
     }
     else
     {
-        for (const float &i: vectorInput)
+        for (const float &i : vectorInput)
         {
             sum += (double)i;
         }
